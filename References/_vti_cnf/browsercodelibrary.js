54d4a271710d14f70e4b041405fcdecac5a9d80e
tomokowitz <tomokowitz@gmail.com>
2016-10-31 22:13:20 -0400

    Creating new Tanis-Actual repo

---
+++ References/_vti_cnf/browsercodelibrary.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|25 Oct 2016 11:42:18 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DevNotebook\\tomokowitz
+vti_modifiedby:SR|TWYCANAL\\okowitz
+vti_timecreated:TR|23 Oct 2016 00:29:13 -0000
+vti_backlinkinfo:VX|okowitz6aOld.htm okowitz6a.htm
+vti_cacheddtm:TX|25 Oct 2016 11:42:18 -0000
+vti_filesize:IR|3223
