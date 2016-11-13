d0d1897df38b2f7f8ffc5163216ecd9d0ce7f0c2
tomokowitz <tomokowitz@gmail.com>
2016-11-12 22:01:01 -0500

    12 Nov hw 8 php

---
+++ okowitz8b.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Untitled 1</title>
+</head>
+
+<body>
+
+<form method="post" 
+      action="ex9.php">
+
+<?php
+  $team1=$_POST["team1"];
+  $team2=$_POST["team2"];
+  $score1=$_POST["score1"];
+  $score2=$_POST["score2"];
+<?php
+
+?>
+
+</body>
+
+</html>
