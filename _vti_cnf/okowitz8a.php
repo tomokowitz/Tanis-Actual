ed9a3a682b988c5abb52c226d5d382bf9b453827
tomokowitz <tomokowitz@gmail.com>
2016-11-13 22:10:41 -0500

    13 nov 10pm

--- _vti_cnf/okowitz8a.php
+++ _vti_cnf/okowitz8a.php
@@ -1,14 +1,14 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|DevNotebook\\tomokowitz
 vti_modifiedby:SR|DevNotebook\\tomokowitz
-vti_timelastmodified:TR|13 Nov 2016 21:34:09 -0000
+vti_timelastmodified:TR|13 Nov 2016 22:37:20 -0000
 vti_timecreated:TR|13 Nov 2016 01:33:47 -0000
 vti_title:SR|Untitled 1
 vti_extenderversion:SR|12.0.0.0
-vti_nexttolasttimemodified:TW|13 Nov 2016 21:22:07 -0000
+vti_nexttolasttimemodified:TW|13 Nov 2016 22:37:04 -0000
 vti_backlinkinfo:VX|
-vti_cacheddtm:TX|13 Nov 2016 21:34:09 -0000
-vti_filesize:IR|210
+vti_cacheddtm:TX|13 Nov 2016 22:37:04 -0000
+vti_filesize:IR|1187
 vti_cachedtitle:SR|Untitled 1
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
--- okowitz8a.php
+++ okowitz8a.php
@@ -9,10 +9,39 @@ Have a php file named <lastname>8a.php that will display the contents of the BAS
 
 
 <?php
-   
-<?php
-
+   $DBConnect = mysql_connect("66.#####", "okowitzt970", "");
+	if (! $DBConnect)
+	{
+  		echo "<P>Database not available";
+	}
+	mysql_select_db("okowitzt970");
+	$SQLstring = "SELECT TEAM_ID, TEAM_NAME, TEAM_CITY FROM BASEBALL;";
+	$QueryResult = mysql_query($SQLstring)
+	     Or die("<P>Unable to execute the query</P>" .
+	          "<P>Error code " . mysql_errno($DBConnect) .
+	            " : " . mysql_error($DBConnect) . "</P>");
+	$num_rows = mysql_num_rows($QueryResult);
+	echo "<P>$num_rows users and passwords</P>";
+	mysql_close($DBConnect);
 ?>
+	
+	<TABLE BORDER="2">
+<TR><TH>Team ID</TH><TH>Team Name</TH><TH>Team Hometown</TH></TR>
+<?php
+$Row = mysql_fetch_row($QueryResult);
+// reads row of query into indexed array 
+// advances result pointer
+do
+{
+  echo "<TR><TD>{$Row[0]}</TD>" .
+    "<TD>{$Row[1]}</TD><TD>{$Row[2]}</TD></TR>";
+  $Row = mysql_fetch_row($QueryResult);
+} while ($Row); // tests for content of null
+mysql_close($DBConnect);
+?>
+</TABLE>
+
+
 
 </body>
 
