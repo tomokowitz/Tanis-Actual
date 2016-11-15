dd246bbcf782f563b52f96029ec4971bda417524
tomokowitz <tomokowitz@gmail.com>
2016-11-14 21:46:06 -0500

    14 NOV 9:45 PM hw 8 updates

--- _vti_cnf/okowitz8b.php
+++ _vti_cnf/okowitz8b.php
@@ -1,14 +1,14 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|DevNotebook\\tomokowitz
 vti_modifiedby:SR|DevNotebook\\tomokowitz
-vti_timelastmodified:TR|13 Nov 2016 21:37:06 -0000
+vti_timelastmodified:TR|15 Nov 2016 02:43:25 -0000
 vti_timecreated:TR|13 Nov 2016 01:33:47 -0000
 vti_title:SR|Untitled 1
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|okowitz8b.htm
-vti_nexttolasttimemodified:TW|13 Nov 2016 21:33:17 -0000
-vti_cacheddtm:TX|13 Nov 2016 21:37:06 -0000
-vti_filesize:IR|353
+vti_nexttolasttimemodified:TW|15 Nov 2016 02:29:51 -0000
+vti_cacheddtm:TX|15 Nov 2016 02:43:25 -0000
+vti_filesize:IR|708
 vti_cachedtitle:SR|Untitled 1
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
--- okowitz8b.php
+++ okowitz8b.php
@@ -14,7 +14,18 @@ Have a php file named <lastname>8b.php that will insert this data into the BASEB
   $teamName=$_POST["teamName"];
   $teamCity=$_POST["teamCity"];
  if ( empty($teamID)) die("<P>No team ID entered");
-<?php
+
+$DBConnect = mysql_connect("localhost", "okowitzt970", "");
+	if (! $DBConnect)
+	{
+  		echo "<P>Database not available";
+	}
+	mysql_select_db("okowitzt970");
+	$SQLstring = "INSERT INTO BASEBALL VALUES(" .
+              $teamID . ",'" . $teamName . "','" . $teamCity . $todayDate . "');";
+	// echo $SQLstring;
+	$QueryResult = mysql_query($SQLstring);
+
 
 ?>
 
