d383be926984ca47ba93c5ba802f27166979c86d
tomokowitz <tomokowitz@gmail.com>
2016-11-17 21:06:28 -0500

    17 nov 9 pm hw8

--- _vti_cnf/okowitz8b.php
+++ _vti_cnf/okowitz8b.php
@@ -1,27 +1,15 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|DevNotebook\\tomokowitz
-<<<<<<< HEAD
-vti_modifiedby:SR|TWYCANAL\\okowitz
-vti_timelastmodified:TR|17 Nov 2016 15:47:23 -0000
-=======
 vti_modifiedby:SR|DevNotebook\\tomokowitz
-vti_timelastmodified:TR|15 Nov 2016 02:43:25 -0000
->>>>>>> origin/master
+vti_timelastmodified:TR|18 Nov 2016 02:04:59 -0000
 vti_timecreated:TR|13 Nov 2016 01:33:47 -0000
 vti_title:SR|Add a Baseball Team
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|okowitz8b.htm
-<<<<<<< HEAD
-vti_nexttolasttimemodified:TW|17 Nov 2016 15:43:52 -0000
-vti_cacheddtm:TX|17 Nov 2016 15:47:23 -0000
-vti_filesize:IR|374
+vti_nexttolasttimemodified:TW|18 Nov 2016 00:46:08 -0000
+vti_cacheddtm:TX|18 Nov 2016 02:04:59 -0000
+vti_filesize:IR|736
 vti_cachedtitle:SR|Add a Baseball Team
-=======
-vti_nexttolasttimemodified:TW|15 Nov 2016 02:29:51 -0000
-vti_cacheddtm:TX|15 Nov 2016 02:43:25 -0000
-vti_filesize:IR|708
-vti_cachedtitle:SR|Untitled 1
->>>>>>> origin/master
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
--- okowitz8b.php
+++ okowitz8b.php
@@ -12,10 +12,8 @@
   $teamName=$_POST["teamName"];
   $teamCity=$_POST["teamCity"];
  if ( empty($teamID)) die("<P>No team ID entered");
-<<<<<<< HEAD
   if ( empty($teamName)) die("<P>No team Name entered");
  if ( empty($teamCity)) die("<P>No team City entered");
-=======
 
 $DBConnect = mysql_connect("localhost", "okowitzt970", "");
 	if (! $DBConnect)
@@ -28,8 +26,6 @@ $DBConnect = mysql_connect("localhost", "okowitzt970", "");
 	// echo $SQLstring;
 	$QueryResult = mysql_query($SQLstring);
 
-
->>>>>>> origin/master
 ?>
 
 </body>
